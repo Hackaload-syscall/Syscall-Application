a2f745863ce43ad84f67fac0cb0e1adb7019fa7f
JiYoungSeok <jiys5164@naver.com>
2017-11-08 17:15:00 +0900

    11/08 Debug

--- Syscall/app/src/main/java/com/example/youngseok/syscall/RunActivity.java
+++ Syscall/app/src/main/java/com/example/youngseok/syscall/RunActivity.java
@@ -29,6 +29,7 @@ public class RunActivity extends AppCompatActivity implements TextToSpeech.OnIni
     String []classificationArr = { "경형", "소형", "중형", "대형", "트럭", "버스" };
     String []colorArr = { "검정", "흰색", "회색", "빨강", "주황", "파랑", "보라", "분홍", "초록", "노랑", "갈색" };
 
+
     CalculateVelocity calculateVelocity;
     DirectionVector directionVector;
 
@@ -45,7 +46,6 @@ public class RunActivity extends AppCompatActivity implements TextToSpeech.OnIni
     private boolean flag = true;
 
     private TextToSpeech myTTS;
-    private Angle angle;
 
     TextView textViewLatitude, textViewLongitude, textViewAccel, textViewVelocity, textViewDirection;
 
@@ -69,7 +69,7 @@ public class RunActivity extends AppCompatActivity implements TextToSpeech.OnIni
 
         textViewLatitude = (TextView) findViewById(R.id.textView_latitude);
         textViewLongitude = (TextView) findViewById(R.id.textView_longitude);
-        textViewAccel = (TextView) findViewById(R.id.textView_accel);
+//        textViewAccel = (TextView) findViewById(R.id.textView_accel);
         textViewVelocity = (TextView) findViewById(R.id.textView_velocity);
         textViewDirection = (TextView) findViewById(R.id.textView_direction);
 
@@ -166,13 +166,18 @@ public class RunActivity extends AppCompatActivity implements TextToSpeech.OnIni
     }
 
     public void runThreads() {
-        MyRunnable myRunnable = new MyRunnable();
-        Thread myThread = new Thread(myRunnable);
-        myThread.setDaemon(true);
-        myThread.start();
+        SendRunnable sendRunnable = new SendRunnable();
+        Thread sendThread = new Thread(sendRunnable);
+        sendThread.setDaemon(true);
+        sendThread.start();
+
+        ReceiveRunnable receiveRunnable = new ReceiveRunnable();
+        Thread receiveThread = new Thread(receiveRunnable);
+        receiveThread.setDaemon(true);
+        receiveThread.start();
     }
 
-    android.os.Handler mainHandler = new android.os.Handler() {
+    android.os.Handler sendHandler = new android.os.Handler() {
         public void handleMessage(Message msg) {
             startLocationService();
 
@@ -190,7 +195,7 @@ public class RunActivity extends AppCompatActivity implements TextToSpeech.OnIni
                     dirLon = directionVector.getDirectionLon() * 100000;
 
                     textViewVelocity.setText(String.format("%.2f", curVelocity) + "km/h");
-                    textViewAccel.setText(String.format("%.2f", curVelocity - prevVelocity) + "km/h^2");
+//                    textViewAccel.setText(String.format("%.2f", curVelocity - prevVelocity) + "km/h^2");
                     textViewDirection.setText("(" + String.format("%.2f", dirLat) + ", " + String.format("%.2f", dirLon) + ")");
 
                     //Update Latitude & Longitude & Acceleration
@@ -203,9 +208,6 @@ public class RunActivity extends AppCompatActivity implements TextToSpeech.OnIni
                             String.valueOf(directionVector.getDirectionLat() * 100000),
                             String.valueOf(directionVector.getDirectionLon() * 100000));
 
-                    GetOtherCarInfo getOtherCarInfo = new GetOtherCarInfo();
-                    getOtherCarInfo.execute("http://52.79.165.228/syscall/prac.php", SplashActivity.serverID);
-
                     prevVelocity = curVelocity;
                 }
             }
@@ -214,16 +216,40 @@ public class RunActivity extends AppCompatActivity implements TextToSpeech.OnIni
         }
     };
 
-    public class MyRunnable implements Runnable {
+    android.os.Handler receiveHandler = new android.os.Handler() {
+        public void handleMessage(Message msg) {
+            GetOtherCarInfo getOtherCarInfo = new GetOtherCarInfo();
+            getOtherCarInfo.execute("http://52.79.165.228/syscall/prac.php", SplashActivity.serverID);
+        }
+    };
+
+    public class SendRunnable implements Runnable {
         @Override
         public void run() {
 
             while(flag) {
                 Message msg = Message.obtain();
-                mainHandler.sendMessage(msg);
+                sendHandler.sendMessage(msg);
 
                 try {
                     Thread.sleep(1000);
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public class ReceiveRunnable implements Runnable {
+        @Override
+        public void run() {
+
+            while(flag) {
+                Message msg = Message.obtain();
+                receiveHandler.sendMessage(msg);
+
+                try {
+                    Thread.sleep(7000);
                 } catch(Exception e) {
                     e.printStackTrace();
                 }
@@ -289,12 +315,21 @@ public class RunActivity extends AppCompatActivity implements TextToSpeech.OnIni
                 );
             }
 
+            int count = 0;
+            String tts = "";
+
             for (OtherCar oc : otherCarsList) {
                 String ocBrand = brandArr[oc.getBrand()];
                 String ocClassification = classificationArr[oc.getClassification()];
                 String ocColor = colorArr[oc.getColor()];
                 String ocBeginner = (oc.getBeginner() == 0 ? "" : "초보운전");
                 String ocDrowsy = (oc.getDrowsy() == 0 ? "" : "졸음운전");
+
+                if(oc.getLatitude() == 0.0 || oc.getLongitude() == 0.0) {
+                    break;
+                }
+
+                count++;
 
                 MyCar myCar = new MyCar(curLat, curLon, dirLat, dirLon);
                 OtherCar otherCar = new OtherCar(oc.getBrand(), oc.getClassification(), oc.getColor(), oc.getLatitude(), oc.getLongitude(),
@@ -303,12 +338,35 @@ public class RunActivity extends AppCompatActivity implements TextToSpeech.OnIni
                 Angle angle = new Angle(myCar, otherCar);
 
                 Log.e("Other Car Info", ocBrand + " " + ocClassification + " " + ocColor + " " + ocBeginner + " " + ocDrowsy + " " + angle.getAngleWithOtherCar());
-                myTTS.speak(ocBrand + ocClassification + ocColor + ocBeginner + ocDrowsy + angle.getAngleWithOtherCar(), TextToSpeech.QUEUE_ADD, null);
+
+                if(count > 2) break;
+
+                tts = tts + angle.getAngleWithOtherCar() + ocBrand + ocClassification + ocColor + ocBeginner + ocDrowsy + "  ";
+            }
+
+            if(count > 2) {
+                String str = "";
+                switch(count-2) {
+                    case 1: str = "한대"; break;
+                    case 2: str = "두대"; break;
+                    case 3: str = "세대"; break;
+                    case 4: str = "네대"; break;
+                    case 5: str = "다섯대"; break;
+
+                    default: str = "다량의"; break;
+                }
+
+                tts = tts + "외 " + str + " 차량이 접근중 입니다.";
+
+            } else if(count != 0){
+                tts = tts + "차량이 접근중 입니다.";
+            } else {
 
             }
 
-        } catch(Exception e) {
+            myTTS.speak(tts, TextToSpeech.QUEUE_ADD, null);
 
+        } catch(Exception e) {
             e.printStackTrace();
         }
     }
